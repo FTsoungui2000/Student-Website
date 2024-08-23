a45c71b5b7cedbb9c587ac8eb228c5985a259ebd
FTsoungui2000 <franckmtsoungui@gmail.com>
2024-08-23 10:32:22 -0500

    Version 0.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,10 @@ class User < ApplicationRecord
 
     validates :email, format: {with:URI::MailTo::EMAIL_REGEXP}, presence: true, uniqueness: true
 
-    # Sends email from UserNailer
+    # Sends email from UserMailer
     MAILER_FROM_EMAIL = "no-reply@example.com"
+
+    PASSWORD_RESET_TOKEN_EXPIRATION = 10.minutes
 
     def confirm!
         update_colums(confirmed_at: Time.current)
@@ -26,10 +28,19 @@ class User < ApplicationRecord
         !confirmed?
     end
 
+    def generate_password_reset_token
+        signed_id expires_in: PASSWORD_RESET_TOKEN_EXPIRATION, purpose: :reset_password
+    end
+
     # Creates a new confirmation token, which is then sends a link (that expires and can't be reused) to the users email
     def send_confirmation_email!
         confirmation_token = generate_confirmation_token
         UserMailer.confirmation(self, confirmation_token).devliver_now
+    end
+    
+    def send_password_reset_email!
+        password_reset_token = generate_password_reset_token
+        UserMailer.password_reset(self, password_reset_token).devliver_now
     end
     
     private
